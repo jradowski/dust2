5e50001fae42bd63da046c1a3d2fcd577bd0bfd5
jradowski <jonasz402@wp.pl>
2024-12-18 10:12:05 +0100

    18.12v4

--- app/page.tsx
+++ app/page.tsx
@@ -21,10 +21,12 @@ export default function AutoScrollingGallery() {
             {/* Hero section */}
             <div className="text-center text-black py-12 bg-blue-300 dark:bg-gradient-to-r dark:from-gray-800 dark:to-gray-900 dark:text-white w-full">
                 <h1 className="lg:text-4xl sm:text-3xl font-bold mb-4 p-2">
-                    Stable Assistant <br></br>
+                    Stable Assistant  </h1>
+                    <h2 className="lg:text-4xl sm:text-3xl font-bold mb-4 p-2">
                     Zarządzanie Stajnią Jeszcze Nigdy Nie Było Tak Proste!<br></br>
                     Wszystko to możliwe bez wychodzenia z domu!
-                </h1>
+                    </h2>
+               
                 <p className="lg:text-lg sm:text-sm my-6">
                     Dołącz do użytkowników, którzy uprościli swoje codzienne zadania dzięki naszej aplikacji!
                 </p>
